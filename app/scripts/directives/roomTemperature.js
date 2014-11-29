434bc3ce987afc3b37cf234924ba18ac17245657
domwoe <dwoerner@ethz.ch>
2014-11-29 18:31:16 +0100

    fix for $digest already in progress warning

--- app/scripts/directives/roomTemperature.js
+++ app/scripts/directives/roomTemperature.js
@@ -254,7 +254,7 @@ angular.module('cirqlApp')
                                 );
                             }
 
-                            scope.$apply();
+                            $timeout(function() {scope.$apply()});
                         }                   
                     };
 
