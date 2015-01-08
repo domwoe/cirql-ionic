2dc569f5ae99826d38e1e953623af24dbc15be2f
domwoe <dwoerner@ethz.ch>
2015-01-08 18:08:06 +0100

    users in menu

--- app/scripts/controllers/sidemenu.js
+++ app/scripts/controllers/sidemenu.js
@@ -1,83 +1,163 @@
 'use strict';
 
 angular.module('cirqlApp')
-.controller('SideMenuCtrl', ['$scope', 'user', '$state', 'simpleLogin', 'netatmoService', 'fbutil',
-    function($scope, user, $state, simpleLogin, netatmoService, fbutil) {
+    .controller('SideMenuCtrl', ['$scope', 'user', '$state', 'simpleLogin', 'netatmoService', 'fbutil', '$ionicPopup',
+        function($scope, user, $state, simpleLogin, netatmoService, fbutil, $ionicPopup) {
 
-        $scope.logout = function() {
-            simpleLogin.logout();
-            $state.go('login');
-        };
+            $scope.logout = function() {
+                simpleLogin.logout();
+                $state.go('login');
+            };
 
-        $scope.room = $state.params.roomId;
+            $scope.room = $state.params.roomId;
 
-        if ($scope.room) {
-            var usesAutoAway = fbutil.syncObject('homes/' + user.uid + '/rooms/' + $scope.room + '/usesAutoAway');
-            usesAutoAway.$bindTo($scope, 'usesAutoAway');
-        
-            var mode = fbutil.syncObject('homes/' + user.uid + '/rooms/' + $scope.room + '/mode');
-            mode.$bindTo($scope, 'mode');
-        }
+            if ($scope.room) {
+                var usesAutoAway = fbutil.syncObject('homes/' + user.uid + '/rooms/' + $scope.room + '/usesAutoAway');
+                usesAutoAway.$bindTo($scope, 'usesAutoAway');
 
-        $scope.goToSchedule = function() {
-            if (window.screen.hasOwnProperty('lockOrientation')) {
-                window.screen.lockOrientation('landscape');
-            }   
-            $state.go('app.schedule', {roomId: $state.params.roomId} );
-        }; 
+                var mode = fbutil.syncObject('homes/' + user.uid + '/rooms/' + $scope.room + '/mode');
+                mode.$bindTo($scope, 'mode');
 
-        $scope.netatmo = function() {
-            if ($state.params.hasOwnProperty('roomId')) {
+                var residents = fbutil.syncArray('homes/' + user.uid + '/residents');
+                $scope.residents = residents;
 
-
-                var room = $state.params.roomId;
-                
-                var promise = netatmoService.getNetatmo(room,user.uid);
-
-                promise.then(function(hasNetatmo) {
-
-                    console.log('hasNetatmo: '+hasNetatmo);
-
-                    if ( hasNetatmo ) {
-                        $state.go('app.netatmo', {roomId: $state.params.roomId});
-                    }
-                    else {
-                        var isConPromise = netatmoService.isConnected(user.uid);
-                        isConPromise.then(function(isConnected) {
-                            console.log('isConnected: ' + isConnected);
-                            if ( isConnected ) {
-                                $state.go('app.addNetatmo', {roomId: $state.params.roomId});
-                            }
-                        },
-                        // No Netato account connected
-                        function(reject) {
-                            netatmoService.authorizeUrl(user.uid).then(function(url) {
-                                window.open(url, '_blank', 'location=yes');
-                            });
-                            
-
-                        });
-                    }    
-                });              
-                    
+                var boundResidents = fbutil.syncObject('homes/' + user.uid + '/rooms/' + $scope.room + '/residents');
+                $scope.boundResidents = boundResidents;
             }
 
-        };
+            function disableAutoAway() {
+                if ($scope.usesAutoAway && $scope.usesAutoAway.$value) {
+                    $scope.usesAutoAway.$value = false;
+                }
+            }
 
-        $scope.isRoom = function() {
-           if ( $state.current.name === 'app.room' ) {
-            return true;
-           }
-           return false;
+            $scope.isBoundResident = function(resident) {
+                return resident.rooms[$scope.room] && resident.allowsGeolocation;
+            };
+
+            $scope.hasBoundResidents = function() {
+                var hasBoundResidents = false;
+
+                for (var resident in boundResidents) {
+                    if (boundResidents[resident] === true || boundResidents[resident] === false) {
+                        
+                            hasBoundResidents = hasBoundResidents || boundResidents[resident];
+                        
+                        
+                    }
+                }
+
+                if (!hasBoundResidents) {
+                    disableAutoAway()
+                }
+
+                return hasBoundResidents;
+            }
+
+            $scope.toggleBoundResident = function(resident) {
+                
+                if (boundResidents === undefined) {
+                    boundResidents = {};
+                } else {
+
+                    if (resident.rooms != undefined) {
+
+                        if (!resident.allowsGeolocation && !resident.rooms[$scope.room]) {
+
+                            $ionicPopup.alert({
+                                template: resident.name + ' {{"NO_GEO_ALERT" | translate}}'
+                            });
+
+                        } else {
+
+                            if (resident.rooms[$scope.room] != undefined) {
+                                resident.rooms[$scope.room] = !resident.rooms[$scope.room];
+                            } else {
+                                resident.rooms[$scope.room] = true;
+                            }
+
+                        }
+
+
+                    } else {
+                        if (resident.allowsGeolocation) {
+                            resident['rooms'] = {};
+                            resident.rooms[$scope.room] = true;
+                        }
+                    }
+                    residents.$save(resident);
+                    boundResidents[resident.$id] = resident.rooms[$scope.room];
+                    boundResidents.$save();
+
+                }
+
+            };
+
+            $scope.goToSchedule = function() {
+                if (window.screen.hasOwnProperty('lockOrientation')) {
+                    window.screen.lockOrientation('landscape');
+                }
+                $state.go('app.schedule', {
+                    roomId: $state.params.roomId
+                });
+            };
+
+            $scope.netatmo = function() {
+                if ($state.params.hasOwnProperty('roomId')) {
+
+
+                    var room = $state.params.roomId;
+
+                    var promise = netatmoService.getNetatmo(room, user.uid);
+
+                    promise.then(function(hasNetatmo) {
+
+                        console.log('hasNetatmo: ' + hasNetatmo);
+
+                        if (hasNetatmo) {
+                            $state.go('app.netatmo', {
+                                roomId: $state.params.roomId
+                            });
+                        } else {
+                            var isConPromise = netatmoService.isConnected(user.uid);
+                            isConPromise.then(function(isConnected) {
+                                    console.log('isConnected: ' + isConnected);
+                                    if (isConnected) {
+                                        $state.go('app.addNetatmo', {
+                                            roomId: $state.params.roomId
+                                        });
+                                    }
+                                },
+                                // No Netato account connected
+                                function(reject) {
+                                    netatmoService.authorizeUrl(user.uid).then(function(url) {
+                                        window.open(url, '_blank', 'location=yes');
+                                    });
+
+
+                                });
+                        }
+                    });
+
+                }
+
+            };
+
+            $scope.isRoom = function() {
+                if ($state.current.name === 'app.room') {
+                    return true;
+                }
+                return false;
+            }
+
+            $scope.isHome = function() {
+                if ($state.current.name === 'app.home') {
+                    return true;
+                }
+                return false;
+            }
+
+
+
         }
-
-        $scope.isHome = function() {
-           if ( $state.current.name === 'app.home' ) {
-            return true;
-           }
-           return false;
-        }
-
-
-
-}]);
+    ]);
