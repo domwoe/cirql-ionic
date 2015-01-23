040a9a8be6ad7f7bde68bffd9aad07f0ae1a077f
domwoe <dom.woe@gmail.com>
2015-01-23 15:44:25 +0100

    don't cut temperature icon

--- app/scripts/directives/roomTemperature.js
+++ app/scripts/directives/roomTemperature.js
@@ -547,7 +547,7 @@ angular.module('cirqlApp')
                 },
                 replace: true,
                 template: '\
-                <svg id="room-temperature" width="100%" height="100%" overflow:"visible" viewBox="0 0 250 250" preserveAspectRatio="xMidYMin" xmlns="http://www.w3.org/2000/svg" >\
+                <svg id="room-temperature" width="100%" height="100%" overflow:"visible" viewBox="0 -5 250 250" preserveAspectRatio="xMidYMin" xmlns="http://www.w3.org/2000/svg" >\
                     <defs>\
                         <linearGradient id="gradient{{ishome}}{{roomid}}" x1="0%" y1="0%" x2="100%" y2="0%">\
                             <stop offset="0%"></stop>\
