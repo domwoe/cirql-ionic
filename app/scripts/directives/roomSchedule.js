92c5b989f8226a6fdd3afef48da0ea087b406fdc
domwoe <dwoerner@ethz.ch>
2015-02-04 18:28:24 +0100

    custom schedule header and overflow

--- app/scripts/directives/roomSchedule.js
+++ app/scripts/directives/roomSchedule.js
@@ -947,10 +947,7 @@ angular.module('cirqlApp')
 
                 },
                 replace: false,
-                template: '\
-                <ion-content class="has-header has-footer" scroll="false" data-tap-disabled="true"> \
-                    <div class="schedule-block"> \
-                        <svg id="room-schedule" width="100%" height="100%" viewBox="0 0 742 330" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">\
+                template: '<svg id="room-schedule" overflow="visible" width="100%" height="100%" viewBox="0 0 742 330" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">\
                             <g id="weekdays"> \
                                 <g id="monday" class="parent"> \
                                     <rect fill="#483e37" fill-opacity="1" width="95" height="30" x="0" y="0" /> \
@@ -995,7 +992,7 @@ angular.module('cirqlApp')
                                     </text> \
                                 </g> \
                                 <g id="sunday" class="parent"> \
-                                    <rect fill="#483e37" fill-opacity="1" width="95" height="30" x="0" y="180" /> \
+                                    <rect fill="#483e37" fill-opacity="1" stroke="#BFBFBF" stroke-width="1" width="95" height="30" x="0" y="180" /> \
                                     <rect fill="#FFFFFF" fill-opacity="1" stroke="#BFBFBF" stroke-width="1" width="650" height="30" x="95" y="180" /> \
                                     <text font-family="Helvetica Neue" font-size="16" font-weight="300" fill="#FFFFFF"> \
                                         <tspan text-anchor="middle" x="47.5" y="205">Sunday</tspan> \
